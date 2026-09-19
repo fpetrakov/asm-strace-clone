f0ca625c2feb62c484e10f66926015d86636c3cb
fpetrakov <jakephin2015@gmail.com>
2026-09-19 21:06:44 +0300

    add syscalls

---
+++ main.asm
@@ -0,0 +1,12 @@
+%define sys_fork 57
+%define sys_ptrace 101
+%define sys_wait4 61
+%define sys_execve 59
+%define sys_write 1
+%define sys_exit 60
+%define sys_process_vm_readv 310
+
+section .text
+default rel
+
+
