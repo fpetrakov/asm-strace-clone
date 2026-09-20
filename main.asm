f5517cdca2ceba268632ec50372a912a1cc15796
fpetrakov <jakephin2015@gmail.com>
2026-09-20 11:18:39 +0300

    print arg, err when no arg

--- main.asm
+++ main.asm
@@ -5,8 +5,51 @@
 %define sys_write 1
 %define sys_exit 60
 %define sys_process_vm_readv 310
+%define stdout 1
+%define stderr 2
+%define exit_success 0
+%define exit_failure 1
+
+%macro ERRMSG 2
+    %1 db %2, 10
+    %1_len equ $ - %1
+%endmacro
+
+section .robata
+    ERRMSG err_no_arg, "Error: must have PROG [ARGS] or -p PID"
+
 
 section .text
-default rel
+    default rel
+    global _start
 
+_start:
+    mov rcx, [rsp]
+    cmp rcx, 2
+    jl .no_argument
 
+    mov rdi, stdout
+    mov rsi, [rsp+16]
+    mov rdx, 4
+    mov rax, sys_write
+    syscall
+
+    mov rdi, exit_success
+    mov rax, sys_exit
+    syscall
+
+    .no_argument:
+        mov rsi, err_no_arg
+        mov rdx, err_no_arg_len
+        call write_err
+        call exit_err
+
+write_err:
+    mov rdi, stderr
+    mov rax, sys_write
+    syscall
+
+exit_err:
+    mov rdi, exit_failure
+    mov rax, sys_exit
+    syscall
